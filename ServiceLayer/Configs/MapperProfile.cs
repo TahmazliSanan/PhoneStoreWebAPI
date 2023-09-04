5670df98b3e9730e0f0d55b81588300a8a72e4f6
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:21:33 +0400

    Create MapperProfile

---
+++ ServiceLayer/Configs/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entities;
+using DTOLayer.Dtos;
+
+namespace ServiceLayer.Configs
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Phone, PhoneDto>();
+            CreateMap<PhoneDto, Phone>();
+        }
+    }
+}
