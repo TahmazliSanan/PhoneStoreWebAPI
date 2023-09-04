4f4b40bab2ca619b64a30aea7de9d07b89f11c48
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:54:48 +0400

    Create IBaseService

---
+++ ServiceLayer/Abstracts/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceLayer.Abstracts
+{
+    public interface IBaseService<TRequestDto, TEntity, TResponseDto>
+    {
+        Task<TResponseDto> CreateAsync(TRequestDto requestDto);
+        Task<IEnumerable<TResponseDto>> GetAllAsync();
+        Task<TResponseDto> GetByIdAsync(int id);
+        Task<TResponseDto> UpdateAsync(TRequestDto requestDto);
+        Task<int> DeleteAsync(int id);
+    }
+}
