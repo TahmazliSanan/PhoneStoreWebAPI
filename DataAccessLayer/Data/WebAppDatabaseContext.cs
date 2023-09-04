1750a2303a41c3ca770822db41b583780a676222
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 15:51:10 +0400

    Add Context

--- DataAccessLayer/Data/WebAppDatabaseContext.cs
+++ DataAccessLayer/Data/WebAppDatabaseContext.cs
@@ -1,6 +1,15 @@
-﻿namespace DataAccessLayer.Data
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data
 {
-    public class WebAppDatabaseContext
+    public class WebAppDatabaseContext : DbContext
     {
+        public WebAppDatabaseContext(DbContextOptions<WebAppDatabaseContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Phone> Phones { get; set; }
     }
 }
