3977be15ecd7fddb21cf6567e238815bff9dc414
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:16:48 +0400

    Change in class

--- DataAccessLayer/Entities/Phone.cs
+++ DataAccessLayer/Entities/Phone.cs
@@ -6,7 +6,7 @@
         public string BrandModel { get; set; }
         public double Price { get; set; }
         public string CityName { get; set; }
-        public DateTime DateOfSharing { get; set; } = DateTime.Now;
+        public DateTime DateOfSharing { get; set; }
         public string? ImagePath { get; set; }
     }
 }
