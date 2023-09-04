5d7488ad896a3260eae426b48888c8db806944ce
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:01:42 +0400

    Edit property

--- DataAccessLayer/Entities/Phone.cs
+++ DataAccessLayer/Entities/Phone.cs
@@ -6,7 +6,7 @@
         public string BrandModel { get; set; }
         public double Price { get; set; }
         public string CityName { get; set; }
-        public DateTime DateOfSharing { get; set; }
+        public DateTime DateOfSharing { get; set; } = DateTime.Now;
         public string? ImagePath { get; set; }
     }
 }
