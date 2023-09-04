ba98e26d50c541d94830ccd68742a4429f732471
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:01:57 +0400

    Add DTOs

---
+++ DTOLayer/Dtos/PhoneDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTOLayer.Dtos
+{
+    public class PhoneDto : DtoBase
+    {
+        [Required(ErrorMessage = "Brand name cannot be empty!")]
+        public string BrandName { get; set; }
+        [Required(ErrorMessage = "Brand model cannot be empty!")]
+        public string BrandModel { get; set; }
+        [Required(ErrorMessage = "Price cannot be empty!")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price cannot be negative number!")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "City name cannot be empty!")]
+        public string CityName { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
