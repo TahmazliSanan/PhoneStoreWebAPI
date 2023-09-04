66888ebcb5748c8b2fdbaa211dddb87848f8fbf4
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:58:41 +0400

    Create PhoneService

---
+++ ServiceLayer/Implementations/PhoneService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using DTOLayer.Dtos;
+using ServiceLayer.Abstracts;
+
+namespace ServiceLayer.Implementations
+{
+    public class PhoneService : BaseService<PhoneDto, Phone, PhoneDto>, IPhoneService
+    {
+        public PhoneService(IMapper mapper, WebAppDatabaseContext dbContext) : base(mapper, dbContext)
+        {
+        }
+    }
+}
