ad8fae812ec39b1b44d1862a88a1c17094c1d4ee
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-04 16:58:23 +0400

    Create IPhoneService

---
+++ ServiceLayer/Abstracts/IPhoneService.cs
@@ -0,0 +1,9 @@
+﻿using DataAccessLayer.Entities;
+using DTOLayer.Dtos;
+
+namespace ServiceLayer.Abstracts
+{
+    public interface IPhoneService : IBaseService<PhoneDto, Phone, PhoneDto>
+    {
+    }
+}
